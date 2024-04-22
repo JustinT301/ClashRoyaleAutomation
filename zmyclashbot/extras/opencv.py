28397271150d39fcc807205e2710bda9b72a781e
JustinT301 <145940518+JustinT301@users.noreply.github.com>
2024-04-21 21:31:45 -0400

    Delete zmyclashbot directory

--- zmyclashbot/extras/opencv.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import numpy as np
-
-golem_img = cv2.imread('golemdeck/mygolem.png', cv2.IMREAD_UNCHANGED)
-nightwitch_img = cv2.imread('golemdeck/mynightwitch.png', cv2.IMREAD_UNCHANGED)
